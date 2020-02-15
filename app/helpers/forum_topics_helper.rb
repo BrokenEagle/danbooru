2ee081d0cdeb58e142993b92f051ad254fe522c3
BrokenEagle <brokeneagle98@yahoo.com>
2020-02-15 22:04:47 +0000

    wip

--- app/helpers/forum_topics_helper.rb
+++ app/helpers/forum_topics_helper.rb
@@ -10,4 +10,12 @@ module ForumTopicsHelper
   def new_forum_topic?(topic, read_forum_topics)
     !read_forum_topics.map(&:id).include?(topic.id)
   end
+
+  def bulk_update_request_counts(topic)
+    requests = []
+    requests << "<span style='color:orange'>Pending:</span> #{topic.pending_bur_count}" if topic.pending_bur_count > 0
+    requests << "<span style='color:green'>Approved:</span> #{topic.approved_bur_count}" if topic.approved_bur_count > 0
+    requests << "<span style='color:red'>Rejected:</span> #{topic.rejected_bur_count}" if topic.rejected_bur_count > 0
+    requests.join(", ").html_safe
+  end
 end
